4aac846dfa0100a65aa76bdd208e41f76cf4a749
IshavdeepSingh <145477825+IshavdeepSingh@users.noreply.github.com>
2025-12-25 18:52:41 -0500

    plot_construction function

--- estimation-pi.py
+++ estimation-pi.py
@@ -1,4 +1,23 @@
 import numpy as np
+import time 
+import matplotlib.pyplot as plt 
+
+def plot_construction():
+    fig, axis = plt.subplots(1, 2, figsize=(10, 5))
+
+    # Generated Dots
+    axis[0].set_title('Random Generated Dots')
+    axis[0].set_aspect('equal')
+    axis[0].set_xlim([-1, 1])
+    axis[0].set_ylim([-1, 1])
+
+    # Plot of Approximation
+    axis[1].set_title('Approximating π ~')
+    axis[1].set_xlim([0, 1000])
+    axis[1].set_ylim([0, 10])
+    axis[1].grid()
+
+    return axis
 
 N_samples = np.arange(1, 1000) # an array of samples from 1 to 1000 
 pi_counter = 0 #this will be our number of dots in the circle 
@@ -14,6 +33,4 @@ for N in N_samples:
 
     
     probability = pi_counter/N_samples 
-
-    print(4*probability)
-
+    approx_pi = 4*probability
