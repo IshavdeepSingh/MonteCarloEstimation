3af969d7a1e8268a454b9b95721c467cc1046ca8
IshavdeepSingh <145477825+IshavdeepSingh@users.noreply.github.com>
2025-12-25 17:15:06 -0500

    Probability

--- estimation-pi.py
+++ estimation-pi.py
@@ -1,3 +1,19 @@
 import numpy as np
 
-N_samples = a.range(1, 1000)
+N_samples = np.arange(1, 1000) # an array of samples from 1 to 1000 
+pi_counter = 0 #this will be our number of dots in the circle 
+
+for N in N_samples:
+    x = np.random.uniform(-1,1)
+    y = np.random.uniform(-1,1)
+
+    #check if the coordinates of this particular dot is within the circle
+
+    if np.sqrt(x**2 + y**2) <= 1: #if the point is within the radius
+        pi_counter += 1  
+
+    
+    probability = pi_counter/N_samples 
+
+    print(4*probability)
+
