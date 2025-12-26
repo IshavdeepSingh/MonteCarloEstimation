36edef4be93c1db655cd6a518ee727b304d8074f
IshavdeepSingh <145477825+IshavdeepSingh@users.noreply.github.com>
2025-12-25 19:40:28 -0500

    Update estimation-pi.py

--- estimation-pi.py
+++ estimation-pi.py
@@ -16,27 +16,28 @@ def plot_construction():
     axis[1].set_ylim([0, 10])
     axis[1].grid()
 
-    return axis()
-plt.show()
+    return axis
 
 axis = plot_construction()
+
+scatter = axis[0].scatter([], [], marker='o', s=5)      # dots plot (updated each time)
+plot, = axis[1].plot([], [], color='black')             # pi approximation line (updated each time)
 
 N_samples = np.arange(1, 1000) # an array of samples from 1 to 1000 
 pi_counter = 0 #this will be our number of dots in the circle 
 
-dot_x = []
-dot_y = []
+dots_x = []
+dots_y = []
 
 dots_color = []
 pi_array = []
-
 
 for N in N_samples:
     x = np.random.uniform(-1,1)
     y = np.random.uniform(-1,1)
 
     dots_x.append(x)
-    dots_x.append(y)
+    dots_y.append(y)
 
     #check if the coordinates of this particular dot is within the circle
 
@@ -48,8 +49,26 @@ for N in N_samples:
         dots_color.append('blue') #if not in circle 
 
     
-    probability = pi_counter/N_samples 
-    approx_pi = 4*probability
+    probability = pi_counter / N  
+    approx_pi = 4 * probability
 
     pi_array.append(approx_pi)
+
+    # Updating the plots
+    if N%100 == 0:
+        scatter.set_offsets(np.column_stack((dots_x, dots_y)))
+        scatter.set_facecolors(dots_color)
+        axis[0].set_title(f"Red Dots: {pi_counter}, Blue Dots:{N-pi_counter}")
+
+        plot.set_data(range(1, N+1), pi_array)
+        plot.set_color('black')
+
+        # Keeping y-limits stable so it doesn't blow up early when approx_pi is still noisy
+        axis[1].set_title(f'Approximating π ~ {approx_pi:.5f}')
+        axis[1].set_xlim([0, max(1000, N)])   
+        axis[1].set_ylim([2.5, 4.0])          # stable view window for pi
+
+        plt.pause(0.01)
+
+plt.show()
 
